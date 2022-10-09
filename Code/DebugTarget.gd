b19af43cf04aab5cf664b0fabb8f8088575782cc
Scribble-Sheep <46016730+Scribble-Sheep@users.noreply.github.com>
2022-10-09 15:37:53 -0500

    WIP refactor debugtarget code
    Start moving to functions for changing speed/distance.
    Start setting up debugtarget's code to be usable as an
    abstract class.

--- Code/DebugTarget.gd
+++ Code/DebugTarget.gd
@@ -8,4 +8,10 @@ export var speed: float = 2.0
 func _process(delta):
 	d += delta
 	position = Vector2(sin(d * speed) * radius,cos(d*speed) * radius)
-	radius -= 1 *delta
+	#radius -= 1 *delta
+	
+func change_speed():
+	pass
+	
+func change_distance():
+	pass
