39ff5bf2271db7d083fb1277468d0058148ca8f9
Scribble-Sheep <46016730+Scribble-Sheep@users.noreply.github.com>
2023-01-07 16:40:16 -0600

    Revert "WIP refactor debugtarget code"
    This reverts commit b19af43cf04aab5cf664b0fabb8f8088575782cc.

--- Code/DebugTarget.gd
+++ Code/DebugTarget.gd
@@ -8,10 +8,4 @@ export var speed: float = 2.0
 func _process(delta):
 	d += delta
 	position = Vector2(sin(d * speed) * radius,cos(d*speed) * radius)
-	#radius -= 1 *delta
-	
-func change_speed():
-	pass
-	
-func change_distance():
-	pass
+	radius -= 1 *delta
